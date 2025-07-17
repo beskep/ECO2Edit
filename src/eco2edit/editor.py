e6a19af8dd4b1750b0d242d1c2dc2744362e9ae5
beskep <beskep@naver.com>
2025-07-17 11:36:04 +0900

    refactor: Eco2Xml, Eco2Editor 분리

--- src/eco2edit/editor.py
+++ src/eco2edit/editor.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
+import dataclasses as dc
 import functools
-from dataclasses import dataclass
 from math import inf
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar
 
-from eco2 import Eco2, Eco2Xml
+from eco2 import Eco2
+from eco2 import Eco2Xml as _Eco2Xml
 from loguru import logger
 
 if TYPE_CHECKING:
@@ -21,7 +22,7 @@ class ElementNotFoundError(ValueError):
     pass
 
 
-@dataclass(frozen=True)
+@dc.dataclass(frozen=True)
 class Area:
     site: float
     """대지면적"""
@@ -40,8 +41,15 @@ class Area:
         return cls(site=f('buildm21'), building=f('buildm22'), floor=f('buildm23'))
 
 
-class Eco2Editor:
-    SURFACE_TYPE = (
+# NOTE 개별 element 수정 함수 등은 Eco2Xml에,
+# 전체 케이스 수정 함수는 Eco2Editor에 구현
+
+
+@dc.dataclass
+class Eco2Xml(_Eco2Xml):
+    insulation_u_threshold: float = 0.1
+
+    SURFACE_TYPE: ClassVar[tuple[str, ...]] = (
         '외벽(벽체)',
         '외벽(지붕)',
         '외벽(바닥)',
@@ -53,44 +61,29 @@ class Eco2Editor:
         '내부창',
         '지중벽',
     )
-    INSULATION_U_THRESHOLD = 0.1
-
-    def __init__(self, src: str | Path | Eco2):
-        self.eco2 = src if isinstance(src, Eco2) else Eco2.read(src)
-        self.xml = Eco2Xml.create(self.eco2)
-
-    def write(self, path: str | Path, *, dsr: bool | None = None):
-        """`.eco`, `.tpl` 파일 저장."""
-        eco2 = Eco2(
-            header=self.eco2.header,
-            ds=self.xml.tostring('DS'),
-            dsr=self.xml.tostring('DSR'),
-        )
-
-        eco2.write(path, dsr=dsr)
 
     @functools.cached_property
     def area(self):
-        desc = next(self.xml.iterfind('tbl_Desc'))
+        desc = next(self.iterfind('tbl_Desc'))
         return Area.create(desc)
 
-    def _surfaces_by_type(self, t: int | str, /):
+    def surfaces_by_type(self, t: int | str, /):
         if isinstance(t, str):
             t = self.SURFACE_TYPE.index(t)
 
-        for e in self.xml.iterfind('tbl_yk'):
+        for e in self.iterfind('tbl_yk'):
             if int(e.findtext('면형태', NOT_FOUND)) == t:
                 yield e
 
-    def _layers(self, pcode: str | None):
-        for e in self.xml.iterfind('tbl_ykdetail'):
+    def layers(self, pcode: str | None):
+        for e in self.iterfind('tbl_ykdetail'):
             if e.findtext('pcode') == pcode:
                 yield e
 
     def set_wall_uvalue(self, wall: _Element, uvalue: float):
         pcode = wall.findtext('code')
 
-        if not (layers := list(self._layers(pcode))):
+        if not (layers := list(self.layers(pcode))):
             msg = (
                 f'벽체 "{wall.findtext("code")}({wall.findtext("설명")})"에 속한 '
                 '`ykdetail`이 존재하지 않음.'
@@ -102,7 +95,7 @@ class Eco2Editor:
         possible_insulations = [
             e
             for e in layers
-            if float(e.findtext('열전도율') or inf) < self.INSULATION_U_THRESHOLD
+            if float(e.findtext('열전도율') or inf) < self.insulation_u_threshold
         ]
         if possible_insulations:
             insulation = max(
@@ -146,21 +139,6 @@ class Eco2Editor:
         # tbl_yk 열관류율 수정
         next(wall.iterfind('열관류율')).text = str(uvalue)
 
-    def set_walls(self, uvalue: float, surface_type: str = '외벽(벽체)'):
-        if not (walls := list(self._surfaces_by_type(surface_type))):
-            if surface_type == '외벽(바닥)':
-                logger.warning('`외벽(바닥)`이 존재하지 않음.')
-                return
-
-            raise ElementNotFoundError(surface_type)
-
-        for w in walls:
-            if w.findtext('code') == '0':
-                continue
-
-            logger.trace('{} {}', surface_type, w.findtext('설명'))
-            self.set_wall_uvalue(wall=w, uvalue=uvalue)
-
     @staticmethod
     def set_window_uvalue(window: _Element, uvalue: float):
         # 창호열관류율 수정
@@ -190,9 +168,40 @@ class Eco2Editor:
         # XXX 테스트 필요
         pcode = window.findtext('code')
         assert pcode is not None
-        for e in self.xml.iterfind('tbl_myoun'):
+        for e in self.iterfind('tbl_myoun'):
             if e.findtext('열관류율2') == pcode:
                 e.find('투과율').text = total  # type: ignore[union-attr]
+
+
+class Eco2Editor:
+    def __init__(self, src: str | Path | Eco2):
+        self.eco2 = src if isinstance(src, Eco2) else Eco2.read(src)
+        self.xml = Eco2Xml.create(self.eco2)
+
+    def write(self, path: str | Path, *, dsr: bool | None = False):
+        """`.eco`, `.tpl` 파일 저장."""
+        eco2 = Eco2(
+            header=self.eco2.header,
+            ds=self.xml.tostring('DS'),
+            dsr=self.xml.tostring('DSR'),
+        )
+        eco2.write(path, dsr=dsr)
+
+    def set_walls(self, uvalue: float, surface_type: str = '외벽(벽체)'):
+        if not (walls := list(self.xml.surfaces_by_type(surface_type))):
+            if surface_type == '외벽(바닥)':
+                logger.warning('`외벽(바닥)`이 존재하지 않음.')
+            else:
+                raise ElementNotFoundError(surface_type)
+
+        for w in walls:
+            if w.findtext('code') == '0':
+                continue
+
+            logger.trace('{} {}', surface_type, w.findtext('설명'))
+            self.xml.set_wall_uvalue(wall=w, uvalue=uvalue)
+
+        return self
 
     def set_windows(
         self,
@@ -204,7 +213,7 @@ class Eco2Editor:
             msg = f'{uvalue=}, {shgc=}'
             raise ValueError(msg)
 
-        if not (windows := list(self._surfaces_by_type(surface_type))):
+        if not (windows := list(self.xml.surfaces_by_type(surface_type))):
             raise ElementNotFoundError(surface_type)
 
         for w in windows:
@@ -212,9 +221,11 @@ class Eco2Editor:
                 continue
 
             if uvalue is not None:
-                self.set_window_uvalue(window=w, uvalue=uvalue)
+                self.xml.set_window_uvalue(window=w, uvalue=uvalue)
             if shgc is not None:
-                self.set_window_shgc(window=w, shgc=shgc)
+                self.xml.set_window_shgc(window=w, shgc=shgc)
+
+        return self
 
     def set_infiltration_rate(self, value: float | str):
         value = str(value)
@@ -222,8 +233,12 @@ class Eco2Editor:
             if e.text is not None:
                 e.text = value
 
+        return self
+
     def set_light_density(self, value: float | str):
         value = str(value)
         for e in self.xml.iterfind('tbl_zone/조명에너지부하율입력치'):
             if e.text is not None:
                 e.text = value
+
+        return self
